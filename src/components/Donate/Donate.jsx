79c2b7db98ecd6d8896fda4d3e93dcb6fe557ad1
Md Saayed <mdsayed.contact@gmail.com>
2023-09-27 19:33:01 +0600

    added the application features on reame.md file

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 
 const Donate = ({ dtn }) => {
-    const { title, picture, category, text_color, btn_bg, card_bg, donate_amount, category_bg } = dtn;
+    const { id, title, picture, category, text_color, btn_bg, card_bg, donate_amount, category_bg } = dtn;
 
     const cardStyle = {
         backgroundColor: `${card_bg}`
@@ -23,14 +24,14 @@ const Donate = ({ dtn }) => {
     return (
         <div style={cardStyle} className="grid grid-cols-6 items-center rounded-md gap-4">
             <div style={bgimageEachItem} className="h-full w-full rounded-l-md col-span-2">
-                
+
             </div>
             <div className="col-span-4 py-4">
                 <p style={ctgTextStyles} className="inline-block py-[2px] px-3 text-sm rounded mb-1">{category}</p>
                 <p style={textColor}>${donate_amount}.00</p>
                 <h2 className="text-[1rem] font-semibold">{title}</h2>
                 <div className="mt-2">
-                    <button style={btnBg} className="text-white px-3 py-[3px] rounded">View Details</button>
+                    <Link  style={btnBg} className="text-white inline-block px-3 py-[3px] rounded" to={`/donation/${id}`}>View Details</Link>
                 </div>
             </div>
         </div>
--- src/pages/Home/ErrorPage.jsx
+++ src/pages/Home/ErrorPage.jsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
         <div className='w-full h-[100vh] flex items-center justify-center'>
             <div>
                 <h1 className='text-[red] text-4xl font-extrabold text-center'>404</h1>
-                <p className='text-center'>Page not found</p>
+                <p className='text-center'>Page not found.</p>
             </div>
         </div>
     );
