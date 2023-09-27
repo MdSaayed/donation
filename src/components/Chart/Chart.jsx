e0fed4068d05b24e9f1fa2f84d38f8a31795ce84
Md Saayed <mdsayed.contact@gmail.com>
2023-09-27 09:57:20 +0600

    Update Pie chart and search functionalities some bug fixed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import Chart from 'react-apexcharts';
+import './Style.css'
 
 const PiChart = () => {
   const totalDonations = 12;
-  const donationsMade = JSON.parse(localStorage.getItem('donation-item'))?.length || 0;
+  const donationReceived = JSON.parse(localStorage.getItem('donation-item'))?.length || 0;
 
-  const chartData = [donationsMade, totalDonations - donationsMade];
+  const chartData = [donationReceived, totalDonations - donationReceived];
 
   const options = {
     labels: ['Your Donation', 'Total Donation'],
     colors: ['#00C49F', '#FF444A'],
     legend: {
       position: 'bottom',
+      markers: {
+        radius: 0, 
+        width: 45,
+        height: 6,
+        strokeWidth: 3,
+      },
+      fontSize: '14',
+      fontWeight: 'bold',
     },
   };
 
   return (
     <React.Fragment>
-      <div className='flex items-center justify-center h-[100vh] w-full'>
+      <div className='flex items-center justify-center h-[90vh] w-full'>
         <Chart
           type='pie'
           width={600}
@@ -31,7 +40,3 @@ const PiChart = () => {
 }
 
 export default PiChart;
-
-
-
-
--- src/components/Donation/Donations.jsx
+++ src/components/Donation/Donations.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import Donation from "./Donation";
 
-const Donations = ({searchText}) => {
+const Donations = ({ searchText }) => {
 
     const [donations, setDonations] = useState([]);
-    const search = searchText;   
+    const search = searchText;
+    const searchTextLowerCase = search.toLowerCase();
 
     useEffect(() => {
         fetch('donation.json')
@@ -18,7 +19,7 @@ const Donations = ({searchText}) => {
             <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto my-20">
                 {
                     donations.filter((dtn) => {
-                        return search?.toLowerCase() === '' ? dtn : dtn?.category?.toLowerCase().includes(search);
+                        return searchTextLowerCase === '' ? dtn : dtn?.category?.toLowerCase().includes(searchTextLowerCase);
                     }).map((dtn, idx) => <Donation key={idx} dtn={dtn} />)
                 }
             </div>
